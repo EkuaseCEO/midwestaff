bd71061482a7583a779c3e3bac876f59ce5ad235
ALBERT IMASUEN <studentxapp@gmail.com>
2024-10-11 10:17:32 -0400

    pushing it

---
+++ partials/_footer.php
@@ -0,0 +1,4 @@
+<div class="topspace"></div>
+<footer>
+    <p>&copy 2024 Made with ❤️ By <a href="https://www.miicotech.com" target="black"><strong> Miicotech </strong></a> | <a href="https://www.miicotech.com" target="blank"><strong>Miicotech Company</strong></a></p>
+</footer>
